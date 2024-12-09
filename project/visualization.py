7e740acd6dee70f38233e652b2177a6954958687
Ongkrab <ong_dkrab@hotmail.com>
2024-12-09 09:21:21 +0100

    Add save result to file

--- project/visualization.py
+++ project/visualization.py
@@ -1,9 +1,14 @@
 import numpy as np
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
+import os
 
-folder_name = "20241204-144607"
-result_folder_path = "./results/" + folder_name + "/"
+folder_name = "20241206-133229"
+result_folder_path = "./hello2/" + folder_name + "/"
+image_folder_path = result_folder_path + "images/"
+
+if not os.path.exists(image_folder_path):
+    os.makedirs(image_folder_path)
 
 reindeer_population = genfromtxt(
     result_folder_path + "reindeer_population.csv", delimiter=","
@@ -34,6 +39,7 @@ plt.xlabel("Time Step")
 plt.ylabel("Population")
 plt.title("Population Dynamics")
 plt.legend()
+plt.savefig(image_folder_path + "population_dynamics.png")
 plt.show()
 
 death_by_age = np.array(death_by_age)
@@ -50,6 +56,7 @@ plt.title("Cause of death")
 plt.xlabel("Time Step")
 plt.ylabel("Total amount")
 plt.legend()
+plt.savefig(image_folder_path + "death_causes.png")
 plt.show()
 
 culling_statistics = np.array(culling_statistics)
@@ -57,4 +64,5 @@ plt.plot(culling_statistics[:, 0], culling_statistics[:, 1])
 plt.title("Culling statistics")
 plt.xlabel("Time Step")
 plt.ylabel("Amount culled each season")
+plt.savefig(image_folder_path + "culling_statistics.png")
 plt.show()

079cb01effb4a5e73b2cfb0bc7387ddb326be893
Ongkrab <ong_dkrab@hotmail.com>
2024-12-15 14:37:26 +0100

    Update average_visualization.py

--- project/average_visualization.py
+++ project/average_visualization.py
@@ -7,7 +7,55 @@ import os
 
 ROOT_PATH = "./results/"
 CONFIG_PATH = "./config.json"
-INTRUSION_INTEREST = [10, 50, 60, 70, 80]
+INTRUSION_INTEREST = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+# FOLDER_NAMES = [
+#     "20241213-144635",
+#     "20241213-144544",
+#     "20241213-144406",
+#     "20241213-144352",
+#     "20241213-144304",
+#     "20241213-144157",
+#     "20241213-144044",
+#     "20241213-144032",
+#     "20241213-143942",
+#     "20241213-143936",
+#     "20241214-233843",
+#     "20241214-233947",
+#     "20241214-234001",
+#     "20241214-234009",
+#     "20241214-234039",
+#     "20241214-234223",
+#     "20241214-234326",
+#     "20241214-234335",
+#     "20241214-234524",
+#     "20241214-234532",
+# ]  # Intrusion Radius 80, Food regeneration 0.0025, 0.0035
+# FOLDER_NAMES = [
+#     "20241213-150007",
+#     "20241213-150011",
+#     "20241213-145710",
+#     "20241213-145620",
+#     "20241213-145534",
+#     "20241213-145447",
+#     "20241213-145408",
+#     "20241213-145342",
+#     "20241213-145323",
+#     "20241213-145300",
+#     "20241214-232758",
+#     "20241214-232843",
+#     "20241214-232921",
+#     "20241214-232942",
+#     "20241214-233017",
+#     "20241214-233043",
+#     "20241214-233058",
+#     "20241214-233113",
+#     "20241214-233131",
+#     "20241214-233151",
+# ]  # Intrusion Radius 70, Food regeneration 0.0025, 0.0035
+# FOLDER_NAMES = [
+#     "20241215-102450",
+#     "20241215-102454",
+# ]
 FOLDER_NAMES = [
     "20241213-131030",
     "20241213-131101",
@@ -99,7 +147,21 @@ FOLDER_NAMES = [
     "20241213-150841",
     "20241213-150935",
     "20241213-150942",
+    "20241214-224857",
+    "20241214-225523",
+    "20241214-225534",
+    "20241214-225540",
+    "20241214-225551",
+    "20241214-225601",
+    "20241214-225606",
+    "20241214-225612",
+    "20241214-225616",
+    "20241214-225622",
 ]
+# FOLDER_NAMES = [
+#     "20241214-225534",
+#     "20241213-142809",
+# ]
 IMAGE_FOLDER_NAME = "merges/images"
 
 
@@ -189,7 +251,12 @@ def read_results(folder_name, root_path, group_by="intrusion.radius"):
 
 
 def average_population_dynamics(
-    df, group_by="intrusion_radius", is_save=False, image_folder_path=IMAGE_FOLDER_NAME
+    df,
+    group_by="intrusion_radius",
+    only_reindeer=False,
+    is_save=False,
+    image_folder_path=IMAGE_FOLDER_NAME,
+    intrusion_interest=INTRUSION_INTEREST,
 ):
     # Group the data by the specified column
     grouped_data = df.groupby(group_by).agg(
@@ -210,16 +277,18 @@ def average_population_dynamics(
     # Plot the population dynamics
     plt.figure(figsize=(12, 8))
     for intrusion_radius in average_reindeer_population.index:
-        if intrusion_radius not in INTRUSION_INTEREST:
+        if intrusion_radius not in intrusion_interest:
             continue
         plt.plot(
             average_reindeer_population[intrusion_radius],
             label=f"Reindeer Population - {label}: {intrusion_radius}",
         )
-        plt.plot(
-            average_predator_population[intrusion_radius],
-            label=f"Predator Population - {label}: {intrusion_radius}",
-        )
+
+        if not only_reindeer:
+            plt.plot(
+                average_predator_population[intrusion_radius],
+                label=f"Predator Population - {label}: {intrusion_radius}",
+            )
 
     max_steps = len(average_reindeer_population[intrusion_radius])
     plt.axvline(
@@ -232,7 +301,7 @@ def average_population_dynamics(
 
     plt.xlabel("Time Step")
     plt.ylabel("Population")
-    plt.title("Population Dynamics")
+    plt.title("Population Dynamics Intrusion Radius")
     plt.legend()
     plt.tight_layout(rect=[0, 0, 0.85, 1])
     if is_save:
@@ -241,6 +310,89 @@ def average_population_dynamics(
 
 
 import matplotlib.pyplot as plt
+
+
+def average_population_dynamics_2axis(
+    df,
+    group_by="intrusion_radius",
+    only_reindeer=False,
+    is_save=False,
+    image_folder_path=IMAGE_FOLDER_NAME,
+    intrusion_interest=INTRUSION_INTEREST,
+):
+    # Group the data by the specified column
+    grouped_data = df.groupby(group_by).agg(
+        {
+            "reindeer_population": "mean",
+            "predator_population": "mean",
+        }
+    )
+
+    average_reindeer_population = grouped_data["reindeer_population"]
+    average_predator_population = grouped_data["predator_population"]
+
+    label = convert_to_title_case(group_by)
+    save_file_name = (
+        f"{ROOT_PATH}{image_folder_path}/average_population_dynamics_{group_by}.svg"
+    )
+
+    # Plot the population dynamics
+    fig, ax1 = plt.subplots(figsize=(10, 5))
+
+    ax2 = ax1.twinx()
+    ax1.set_xlabel("Time Step")
+    ax1.set_ylabel("Reindeer Population", color="tab:blue")
+    ax2.set_ylabel("Predator Population", color="tab:red")
+    colors = [
+        "tab:blue",
+        "tab:red",
+        "tab:green",
+        "tab:orange",
+        "tab:purple",
+        "tab:brown",
+    ]
+    i = 0
+    for intrusion_radius in average_reindeer_population.index:
+        if intrusion_radius not in intrusion_interest:
+            continue
+        ax1.plot(
+            average_reindeer_population[intrusion_radius],
+            label=f"Reindeer Population - {label}: {intrusion_radius}",
+            color=colors[i],
+        )
+
+        if not only_reindeer:
+            ax2.plot(
+                average_predator_population[intrusion_radius],
+                label=f"Predator Population - {label}: {intrusion_radius}",
+                color=colors[i + 1],
+            )
+        i += 2
+    ax1.legend(loc="upper left")
+    ax2.legend(loc="upper right")
+    ax1.set_ylim(0, 350)
+    ax2.set_ylim(0, 50)
+    ax1.set_xlim(0, 10000)
+    ax2.set_ylabel("Predator Population")
+
+    max_steps = len(average_reindeer_population[intrusion_radius])
+    ax1.axvline(
+        x=max_steps / 2,
+        color="grey",
+        linestyle="--",
+        linewidth=2,
+        label="Intrusion added",
+    )
+
+    plt.xlabel("Time Step")
+
+    plt.title("Average Population Dynamics for Intrusion Radius: 60")
+    # plt.title("Population Dynamics for Intrusion Radius: 60")
+    plt.tight_layout()
+    # plt.tight_layout(rect=[0, 0, 0.85, 1])
+    if is_save:
+        plt.savefig(save_file_name)
+    plt.show()
 
 
 def error_bar_population_dynamics(
@@ -315,6 +467,7 @@ def average_culling_statistics(
     max_steps=10000,
     is_save=False,
     image_folder_path="",
+    intrusion_interest=INTRUSION_INTEREST,
 ):
     # Merge the culling statistics with the configuration data to get the intrusion radius
     # Group the data by the specified column and calculate the mean of the second column
@@ -335,7 +488,7 @@ def average_culling_statistics(
     plt.figure(figsize=(12, 8))
 
     for intrusion_radius in average_culling_statistic.index:
-        if intrusion_radius not in INTRUSION_INTEREST:
+        if intrusion_radius not in intrusion_interest:
             continue
         x_value = average_culling_statistic[intrusion_radius][:, 0]
         y_value = average_culling_statistic[intrusion_radius][:, 1]
@@ -356,10 +509,269 @@ def average_culling_statistics(
     plt.show()
 
 
+def average_culling_cause_statistics(
+    df,
+    group_by="intrusion_radius",
+    max_steps=10000,
+    is_save=False,
+    image_folder_path="",
+    intrusion_interest=INTRUSION_INTEREST,
+):
+    # Merge the culling statistics with the configuration data to get the intrusion radius
+    # Group the data by the specified column and calculate the mean of the second column
+
+    grouped_data = df.groupby(group_by).agg(
+        {
+            "culling_statistics": ["mean"],
+            "death_by_culling": ["mean"],
+            "death_by_predator": ["mean"],
+            "death_by_starvation": ["mean"],
+        }
+    )
+
+    average_culling_statistic = grouped_data["culling_statistics"]["mean"]
+    average_death_by_culling = grouped_data["death_by_culling"]["mean"]
+    average_death_by_predator = grouped_data["death_by_predator"]["mean"]
+    average_death_by_starvation = grouped_data["death_by_starvation"]["mean"]
+
+    label = convert_to_title_case(group_by)
+    save_file_name = (
+        f"{ROOT_PATH}{image_folder_path}/average_culling_statistics_{group_by}.svg"
+    )
+
+    plt.figure(figsize=(12, 8))
+
+    for intrusion_radius in average_culling_statistic.index:
+        if intrusion_radius not in intrusion_interest:
+            continue
+        x_value = average_culling_statistic[intrusion_radius][:, 0]
+        y_value = average_culling_statistic[intrusion_radius][:, 1]
+
+        plt.plot(  # Plot the average culling statistics
+            x_value,
+            y_value,
+            label=f"Culling Statistic - {label}: {intrusion_radius}",
+        )
+
+        x_culling = average_death_by_culling[intrusion_radius][:, 0]
+        y_culling = average_death_by_culling[intrusion_radius][:, 1]
+        plt.plot(
+            x_culling,
+            y_culling,
+            label=f"Death by Culling - {label}: {intrusion_radius}",
+        )
+
+        x_predator = average_death_by_predator[intrusion_radius][:, 0]
+        y_predator = average_death_by_predator[intrusion_radius][:, 1]
+        plt.plot(
+            x_predator,
+            y_predator,
+            label=f"Death by Predator - {label}: {intrusion_radius}",
+        )
+
+        x_starvation = average_death_by_starvation[intrusion_radius][:, 0]
+        y_starvation = average_death_by_starvation[intrusion_radius][:, 1]
+        plt.plot(
+            x_starvation,
+            y_starvation,
+            label=f"Death by Starvation - {label}: {intrusion_radius}",
+        )
+
+    plt.xlabel(label)
+    plt.ylabel("Average Culling Statistic")
+    plt.title(f"Average Culling Cause Statistic by {label}")
+    plt.legend()
+    plt.grid(True)
+
+    if is_save:
+        plt.savefig(save_file_name)
+    plt.show()
+
+
+def create_predator_death_by_age(
+    df,
+    group_by="intrusion_radius",
+    max_steps=10000,
+    is_save=False,
+    image_folder_path="",
+    intrusion_interest=INTRUSION_INTEREST,
+):
+
+    grouped_data = df.groupby(group_by).agg(
+        {
+            "predator_death_by_age": ["mean"],
+            "predator_death_by_starvation": ["mean"],
+        }
+    )
+
+    average_predator_death_by_age = grouped_data["predator_death_by_age"]["mean"]
+    average_predator_death_by_starvation = grouped_data["predator_death_by_starvation"][
+        "mean"
+    ]
+
+    label = convert_to_title_case(group_by)
+    save_file_name = (
+        f"{ROOT_PATH}{image_folder_path}/average_predator_death_{group_by}.svg"
+    )
+
+    plt.figure(figsize=(12, 8))
+
+    # for intrusion_radius in average_predator_death_by_age.index:
+    #     if intrusion_radius not in intrusion_interest:
+    #         continue
+    #     x_age_value = average_predator_death_by_age[intrusion_radius][:, 0]
+    #     y_age_value = average_predator_death_by_age[intrusion_radius][:, 1]
+
+    #     plt.plot(  # Plot the average culling statistics
+    #         x_age_value,
+    #         y_age_value,
+    #         label=f"Old age - {label}: {intrusion_radius}",
+    #     )
+
+    for intrusion_radius in average_predator_death_by_starvation.index:
+        if intrusion_radius not in intrusion_interest:
+            continue
+        x_starvation_value = average_predator_death_by_starvation[intrusion_radius][
+            :, 0
+        ]
+        y_starvation_value = average_predator_death_by_starvation[intrusion_radius][
+            :, 1
+        ]
+
+        plt.plot(  # Plot the average culling statistics
+            x_starvation_value,
+            y_starvation_value,
+            label=f"Starvation- {label}: {intrusion_radius}",
+        )
+
+    plt.axvline(
+        x=max_steps / 2,
+        color="grey",
+        linestyle="--",
+        linewidth=2,
+        label="Intrusion added",
+    )
+    # plt.yscale("log")
+    plt.title("Predator cause of death")
+    plt.xlabel("Time Step")
+    plt.ylabel("Total amount")
+    plt.legend()
+    if is_save:
+        plt.savefig(save_file_name)
+
+    plt.show()
+
+
+def create_culling_drop_scatter_plot(
+    df,
+    group_by="intrusion_radius",
+    max_steps=10000,
+    is_save=False,
+    image_folder_path="",
+    intrusion_interest=INTRUSION_INTEREST,
+):
+
+    grouped_data = df.groupby(group_by).agg(
+        {
+            "culling_statistics": ["mean"],
+        }
+    )
+    average_culling_cause_statistics = grouped_data["culling_statistics"]["mean"]
+
+    culling_drop_percentages = []
+    intrusion_radii = []
+
+    for intrusion_radius in average_culling_cause_statistics.index:
+        if intrusion_radius not in intrusion_interest:
+            continue
+        culling_statistics = average_culling_cause_statistics[intrusion_radius]
+        half_steps = int(len(culling_statistics) / 2)
+        second_half_culling = culling_statistics[half_steps:]
+        mean_second_half_culling = np.mean(second_half_culling, axis=0)
+
+        culling_drop_percentages.append(mean_second_half_culling[1])
+        intrusion_radii.append(intrusion_radius)
+
+    # Create the scatter plot
+    plt.figure(figsize=(8, 5))
+    plt.scatter(
+        intrusion_radii, culling_drop_percentages, color="blue", label="Culling Drop"
+    )
+    plt.xlabel("Intrusion Radius")
+    plt.ylabel("Mean Culling in Second Half")
+    plt.title("Mean Culling in Second Half vs Intrusion Radius")
+    plt.axhline(0, color="grey", linestyle="--", linewidth=1, alpha=0.8)
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+
+    if is_save:
+        plt.savefig(image_folder_path + "culling_drop_vs_intrusion_radius.png")
+
+    plt.show()
+
+    # for k, folder_names_current in enumerate(FOLDER_NAMES_LIST):
+    #     # Calculate average culling for the last 5000 steps
+    #     total_culling_last_5000 = []
+    #     intrusion_radius = None
+    #     for folder_name in folder_names_current:
+    #         culling_statistics = genfromtxt(
+    #             f"{ROOT_PATH}{folder_name}/culling_statistics.csv", delimiter=","
+    #         )
+    #         half_steps = int(len(culling_statistics) / 2)
+    #         avg_culling = np.sum(culling_statistics[-half_steps:, 1])
+    #         total_culling_last_5000.append(avg_culling)
+
+    #         # Get the intrusion radius
+    #         config = helper.load_config(f"{ROOT_PATH}{folder_name}/config.json")
+    #         intrusion_radius = config["intrusion"]["radius"]
+
+    #     # Average across seeds
+    #     avg_culling_with_intrusion = np.sum(total_culling_last_5000)
+
+    #     if k == 0:
+    #         baseline_culling = avg_culling_with_intrusion  # Intrusion radius = 0
+
+    #     culling_drop_percent = (
+    #         (baseline_culling - avg_culling_with_intrusion) / baseline_culling
+    #     ) * 100
+    #     culling_drop_percentages.append(culling_drop_percent)
+    #     intrusion_radii.append(intrusion_radius)
+
+
 if __name__ == "__main__":
     df = read_results(FOLDER_NAMES, ROOT_PATH, group_by="intrusion.radius")
-    average_population_dynamics(df, group_by="intrusion_radius", is_save=True)
-    error_bar_population_dynamics(df, group_by="intrusion_radius", is_save=True)
-    average_culling_statistics(
-        df, group_by="intrusion_radius", is_save=True
-    )  # Haven't Average Culling Statistics yet
+    intrusion_interest = [0, 60]
+
+    average_population_dynamics_2axis(
+        df,
+        group_by="intrusion_radius",
+        is_save=True,
+        only_reindeer=False,
+        intrusion_interest=intrusion_interest,
+        image_folder_path=IMAGE_FOLDER_NAME,
+    )
+    # average_culling_statistics(
+    #     df,
+    #     group_by="intrusion_radius",
+    #     is_save=True,
+    #     intrusion_interest=intrusion_interest,
+    # )  # Haven't Average Culling Statistics ye
+
+    # average_population_dynamics(
+    #     df,
+    #     group_by="food_regeneration_rate",
+    #     is_save=True,
+    # )
+    # error_bar_population_dynamics(df, group_by="intrusion_radius", is_save=True)
+
+    # create_predator_death_by_age(df, group_by="intrusion_radius", is_save=True)
+    # intrusion_interest = [
+    #     70,
+    # ]
+    # average_culling_cause_statistics(
+    #     df,
+    #     group_by="intrusion_radius",
+    #     is_save=True,
+    #     intrusion_interest=intrusion_interest,
+    # )

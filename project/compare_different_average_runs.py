c9ab8cde27bcada3253142e4a417acb733e792e0
Ongkrab <ong_dkrab@hotmail.com>
2024-12-16 10:34:38 +0100

    Update to show scratter plot

--- project/compare_different_average_runs.py
+++ project/compare_different_average_runs.py
@@ -6,35 +6,230 @@ import os
 
 ROOT_PATH = "./results/"
 CONFIG_PATH = "./config.json"
-ROOT_PATH3 = "./results/Ong_results/results/"
+ROOT_PATH3 = "./results/"
 
-FOLDER_NAMES_LIST1 = [["seed1_intrusion0","seed2_intrusion0","seed3_intrusion0","seed4_intrusion0", "seed5_intrusion0"],
-                     ["seed1_intrusion40","seed2_intrusion40","seed3_intrusion40","seed4_intrusion40", "seed5_intrusion40"],
-                     ["seed1_intrusion50","seed2_intrusion50","seed3_intrusion50","seed4_intrusion50", "seed5_intrusion50"],
-                     ["seed1_intrusion60","seed2_intrusion60","seed3_intrusion60","seed4_intrusion60", "seed5_intrusion60"],
-                     ["seed1_intrusion70","seed2_intrusion70","seed3_intrusion70","seed4_intrusion70", "seed5_intrusion70"],
-                     ["seed1_intrusion80","seed2_intrusion80","seed3_intrusion80","seed4_intrusion80", "seed5_intrusion80"]]
-
-FOLDER_NAMES_LIST2 = [["seed1_intrusion0","seed2_intrusion0","seed3_intrusion0","seed4_intrusion0", "seed5_intrusion0"],
-                     ["seed1_intrusion20","seed2_intrusion20","seed3_intrusion20","seed4_intrusion20", "seed5_intrusion20"],
-                     ["seed1_intrusion40","seed2_intrusion40","seed3_intrusion40","seed4_intrusion40", "seed5_intrusion40"],
-                     ["seed1_intrusion50","seed2_intrusion50","seed3_intrusion50","seed4_intrusion50", "seed5_intrusion50"],
-                     ["seed1_intrusion60","seed2_intrusion60","seed3_intrusion60","seed4_intrusion60", "seed5_intrusion60"],
-                     ["seed1_intrusion70","seed2_intrusion70","seed3_intrusion70","seed4_intrusion70", "seed5_intrusion70"],
-                     ["seed1_intrusion80","seed2_intrusion80","seed3_intrusion80","seed4_intrusion80", "seed5_intrusion80"],]
-
-FOLDER_NAMES_LIST3 = [
-    ["seed1_intrusion10", "seed3_intrusion10", "seed5_intrusion10", "seed7_intrusion10", "seed9_intrusion10", "seed11_intrusion10", "seed13_intrusion10", "seed15_intrusion10", "seed17_intrusion10", "seed19_intrusion10"],
-    ["seed1_intrusion20", "seed3_intrusion20", "seed5_intrusion20", "seed7_intrusion20", "seed9_intrusion20", "seed11_intrusion20", "seed13_intrusion20", "seed15_intrusion20", "seed17_intrusion20", "seed19_intrusion20"],
-    ["seed1_intrusion30", "seed3_intrusion30", "seed5_intrusion30", "seed7_intrusion30", "seed9_intrusion30", "seed11_intrusion30", "seed13_intrusion30", "seed15_intrusion30", "seed17_intrusion30", "seed19_intrusion30"],
-    ["seed1_intrusion40", "seed3_intrusion40", "seed5_intrusion40", "seed7_intrusion40", "seed9_intrusion40", "seed11_intrusion40", "seed13_intrusion40", "seed15_intrusion40", "seed17_intrusion40", "seed19_intrusion40"],
-    ["seed1_intrusion50", "seed3_intrusion50", "seed5_intrusion50", "seed7_intrusion50", "seed9_intrusion50", "seed11_intrusion50", "seed13_intrusion50", "seed15_intrusion50", "seed17_intrusion50", "seed19_intrusion50"],
-    ["seed1_intrusion60", "seed3_intrusion60", "seed5_intrusion60", "seed7_intrusion60", "seed9_intrusion60", "seed11_intrusion60", "seed13_intrusion60", "seed15_intrusion60", "seed17_intrusion60", "seed19_intrusion60"],
-    ["seed1_intrusion70", "seed3_intrusion70", "seed5_intrusion70", "seed7_intrusion70", "seed9_intrusion70", "seed11_intrusion70", "seed13_intrusion70", "seed15_intrusion70", "seed17_intrusion70", "seed19_intrusion70"],
-    ["seed1_intrusion80", "seed3_intrusion80", "seed5_intrusion80", "seed7_intrusion80", "seed9_intrusion80", "seed11_intrusion80", "seed13_intrusion80", "seed15_intrusion80", "seed17_intrusion80", "seed19_intrusion80"]
+FOLDER_NAMES_LIST1 = [
+    [
+        "seed1_intrusion0",
+        "seed2_intrusion0",
+        "seed3_intrusion0",
+        "seed4_intrusion0",
+        "seed5_intrusion0",
+    ],
+    [
+        "seed1_intrusion40",
+        "seed2_intrusion40",
+        "seed3_intrusion40",
+        "seed4_intrusion40",
+        "seed5_intrusion40",
+    ],
+    [
+        "seed1_intrusion50",
+        "seed2_intrusion50",
+        "seed3_intrusion50",
+        "seed4_intrusion50",
+        "seed5_intrusion50",
+    ],
+    [
+        "seed1_intrusion60",
+        "seed2_intrusion60",
+        "seed3_intrusion60",
+        "seed4_intrusion60",
+        "seed5_intrusion60",
+    ],
+    [
+        "seed1_intrusion70",
+        "seed2_intrusion70",
+        "seed3_intrusion70",
+        "seed4_intrusion70",
+        "seed5_intrusion70",
+    ],
+    [
+        "seed1_intrusion80",
+        "seed2_intrusion80",
+        "seed3_intrusion80",
+        "seed4_intrusion80",
+        "seed5_intrusion80",
+    ],
 ]
 
+FOLDER_NAMES_LIST2 = [
+    [
+        "seed1_intrusion0",
+        "seed2_intrusion0",
+        "seed3_intrusion0",
+        "seed4_intrusion0",
+        "seed5_intrusion0",
+    ],
+    [
+        "seed1_intrusion20",
+        "seed2_intrusion20",
+        "seed3_intrusion20",
+        "seed4_intrusion20",
+        "seed5_intrusion20",
+    ],
+    [
+        "seed1_intrusion40",
+        "seed2_intrusion40",
+        "seed3_intrusion40",
+        "seed4_intrusion40",
+        "seed5_intrusion40",
+    ],
+    [
+        "seed1_intrusion50",
+        "seed2_intrusion50",
+        "seed3_intrusion50",
+        "seed4_intrusion50",
+        "seed5_intrusion50",
+    ],
+    [
+        "seed1_intrusion60",
+        "seed2_intrusion60",
+        "seed3_intrusion60",
+        "seed4_intrusion60",
+        "seed5_intrusion60",
+    ],
+    [
+        "seed1_intrusion70",
+        "seed2_intrusion70",
+        "seed3_intrusion70",
+        "seed4_intrusion70",
+        "seed5_intrusion70",
+    ],
+    [
+        "seed1_intrusion80",
+        "seed2_intrusion80",
+        "seed3_intrusion80",
+        "seed4_intrusion80",
+        "seed5_intrusion80",
+    ],
+]
+
+FOLDER_NAMES_LIST3 = (
+    [
+        "20241214-224857",
+        "20241214-225523",
+        "20241214-225534",
+        "20241214-225540",
+        "20241214-225551",
+        "20241214-225601",
+        "20241214-225606",
+        "20241214-225612",
+        "20241214-225616",
+        "20241214-225622",
+    ],
+    [
+        "20241213-131030",
+        "20241213-131101",
+        "20241213-131132",
+        "20241213-131226",
+        "20241213-131258",
+        "20241213-131325",
+        "20241213-131337",
+        "20241213-131437",
+        "20241213-131920",
+        "20241213-131928",
+    ],
+    [
+        "20241213-132148",
+        "20241213-132224",
+        "20241213-132327",
+        "20241213-132348",
+        "20241213-132521",
+        "20241213-132757",
+        "20241213-132928",
+        "20241213-133002",
+        "20241213-133042",
+        "20241213-133127",
+    ],
+    [
+        "20241213-133243",
+        "20241213-133713",
+        "20241213-133747",
+        "20241213-133836",
+        "20241213-133918",
+        "20241213-134016",
+        "20241213-134115",
+        "20241213-134225",
+        "20241213-134339",
+        "20241213-134500",
+    ],
+    [
+        "20241213-134632",
+        "20241213-134733",
+        "20241213-134838",
+        "20241213-135031",
+        "20241213-135110",
+        "20241213-135657",
+        "20241213-135717",
+        "20241213-135728",
+        "20241213-140110",
+        "20241213-140221",
+    ],
+    [
+        "20241213-141231",
+        "20241213-141335",
+        "20241213-141424",
+        "20241213-141429",
+        "20241213-141620",
+        "20241213-141638",
+        "20241213-141649",
+        "20241213-141656",
+        "20241213-142033",
+        "20241213-142103",
+    ],
+    [
+        "20241213-142558",
+        "20241213-142705",
+        "20241213-142809",
+        "20241213-142829",
+        "20241213-143009",
+        "20241213-143050",
+        "20241213-143109",
+        "20241213-143128",
+        "20241213-143152",
+        "20241213-143158",
+    ],
+    [
+        "20241213-143936",
+        "20241213-143942",
+        "20241213-144032",
+        "20241213-144044",
+        "20241213-144157",
+        "20241213-144304",
+        "20241213-144352",
+        "20241213-144406",
+        "20241213-144544",
+        "20241213-144635",
+    ],
+    [
+        "20241213-145300",
+        "20241213-145323",
+        "20241213-145342",
+        "20241213-145408",
+        "20241213-145447",
+        "20241213-145534",
+        "20241213-145620",
+        "20241213-145710",
+        "20241213-150007",
+        "20241213-150011",
+    ],
+    [
+        "20241213-150248",
+        "20241213-150313",
+        "20241213-150403",
+        "20241213-150414",
+        "20241213-150542",
+        "20241213-150642",
+        "20241213-150703",
+        "20241213-150841",
+        "20241213-150935",
+        "20241213-150942",
+    ],
+)
+
 IMAGE_FOLDER_NAME = "images"
+
 
 def create_population_dynamics_multi_run(
     FOLDER_NAMES_LIST,
@@ -43,7 +238,6 @@ def create_population_dynamics_multi_run(
     is_save=False,
     image_folder_path="",
 ):
-    
     """
     Visualize population dynamics for multiple runs on the same graph.
 
@@ -58,24 +252,34 @@ def create_population_dynamics_multi_run(
     plt.figure(figsize=(10, 5))
 
     for folder_names_current in FOLDER_NAMES_LIST:
-        reindeer_population=None
-        predator_population=None
+        reindeer_population = None
+        predator_population = None
         for i, folder_name in enumerate(folder_names_current):
-            reindeer_population_temp = genfromtxt(f"{ROOT_PATH}{folder_name}/reindeer_population.csv", delimiter=',')
+            reindeer_population_temp = genfromtxt(
+                f"{ROOT_PATH}{folder_name}/reindeer_population.csv", delimiter=","
+            )
             if reindeer_population is None:
                 reindeer_population = np.zeros_like(reindeer_population_temp)
-            reindeer_population+=reindeer_population_temp
-            predator_population_temp = genfromtxt(f"{ROOT_PATH}{folder_name}/predator_population.csv", delimiter=',')
+            reindeer_population += reindeer_population_temp
+            predator_population_temp = genfromtxt(
+                f"{ROOT_PATH}{folder_name}/predator_population.csv", delimiter=","
+            )
             if predator_population is None:
                 predator_population = np.zeros_like(predator_population_temp)
-            predator_population+=predator_population_temp
-            
+            predator_population += predator_population_temp
+
             config = helper.load_config(f"{ROOT_PATH}{folder_name}/config.json")
             intrusion_radius = config["intrusion"]["radius"]
-        reindeer_population/=len(folder_names_current)
-        predator_population/=len(folder_names_current)
-        plt.plot(reindeer_population, label=f"Reindeer Population - Intrusion Radius: {intrusion_radius}")
-        plt.plot(predator_population, label=f"Predator Population - Intrusion Radius: {intrusion_radius}")              
+        reindeer_population /= len(folder_names_current)
+        predator_population /= len(folder_names_current)
+        plt.plot(
+            reindeer_population,
+            label=f"Reindeer Population - Intrusion Radius: {intrusion_radius}",
+        )
+        plt.plot(
+            predator_population,
+            label=f"Predator Population - Intrusion Radius: {intrusion_radius}",
+        )
 
     plt.axvline(
         x=max_steps / 2,
@@ -84,35 +288,44 @@ def create_population_dynamics_multi_run(
         linewidth=2,
         label="Intrusion added",
     )
-    
+
     plt.xlabel("Time Step")
     plt.ylabel("Population")
-    plt.title(f"Average Population Dynamics over {len(FOLDER_NAMES_LIST[0])} simulation pairs")
+    plt.title(
+        f"Average Population Dynamics over {len(FOLDER_NAMES_LIST[0])} simulation pairs"
+    )
     plt.legend()
-    plt.tight_layout()#rect=[1, 1, 0, 0])
+    plt.tight_layout()  # rect=[1, 1, 0, 0])
     plt.show()
 
+
 def create_culling_statistics_multi_run(
-        FOLDER_NAMES_LIST,
-        ROOT_PATH,
-        max_steps=10000,
-        is_save=False,
-        image_folder_path="",
+    FOLDER_NAMES_LIST,
+    ROOT_PATH,
+    max_steps=10000,
+    is_save=False,
+    image_folder_path="",
 ):
     plt.figure(figsize=(10, 5))
 
     for folder_names_current in FOLDER_NAMES_LIST:
         culling_statistics = None
         for i, folder_name in enumerate(folder_names_current):
-            culling_statistics_temp = genfromtxt(f"{ROOT_PATH}{folder_name}/culling_statistics.csv", delimiter=',')
+            culling_statistics_temp = genfromtxt(
+                f"{ROOT_PATH}{folder_name}/culling_statistics.csv", delimiter=","
+            )
             if culling_statistics is None:
                 culling_statistics = np.zeros_like(culling_statistics_temp)
-            culling_statistics+=culling_statistics_temp
+            culling_statistics += culling_statistics_temp
             config = helper.load_config(f"{ROOT_PATH}{folder_name}/config.json")
             intrusion_radius = config["intrusion"]["radius"]
-        culling_statistics/=len(folder_names_current)
-        plt.plot(culling_statistics[:, 0], culling_statistics[:, 1], label=f"Culling Statistics - Intrusion Radius: {intrusion_radius}")
-    
+        culling_statistics /= len(folder_names_current)
+        plt.plot(
+            culling_statistics[:, 0],
+            culling_statistics[:, 1],
+            label=f"Culling Statistics - Intrusion Radius: {intrusion_radius}",
+        )
+
     plt.axvline(
         x=max_steps / 2,
         color="grey",
@@ -123,18 +336,22 @@ def create_culling_statistics_multi_run(
 
     plt.xlabel("Time Step")
     plt.ylabel("Culling Statistics")
-    plt.title(f"Average Culling Statistics over {len(FOLDER_NAMES_LIST[0])} simulation pairs")
+    plt.title(
+        f"Average Culling Statistics over {len(FOLDER_NAMES_LIST[0])} simulation pairs"
+    )
     plt.legend()
     plt.tight_layout()
     plt.show()
-    
+
     dif_list = []
     for k in range(len(FOLDER_NAMES_LIST)):
-        dif=0
+        dif = 0
         for i, folder_name in enumerate(FOLDER_NAMES_LIST[k]):
-            death_by_culling = genfromtxt(f"{ROOT_PATH}{folder_name}/death_by_culling.csv", delimiter=',')
-            dif+=death_by_culling[250][1]-death_by_culling[126][1] 
-        dif/=len(FOLDER_NAMES_LIST[k])
+            death_by_culling = genfromtxt(
+                f"{ROOT_PATH}{folder_name}/death_by_culling.csv", delimiter=","
+            )
+            dif += death_by_culling[250][1] - death_by_culling[126][1]
+        dif /= len(FOLDER_NAMES_LIST[k])
         dif_list.append(dif)
 
     print(dif_list)
@@ -174,7 +391,8 @@ def create_culling_drop_scatter_plot(
 
         # Average across seeds
         avg_culling_with_intrusion = np.sum(total_culling_last_5000)
-
+        std_culling_with_intrusion = np.std(total_culling_last_5000)
+        std_culling_percentage = []
         if k == 0:
             baseline_culling = avg_culling_with_intrusion  # Intrusion radius = 0
         else:
@@ -182,17 +400,36 @@ def create_culling_drop_scatter_plot(
                 (baseline_culling - avg_culling_with_intrusion) / baseline_culling
             ) * 100
             culling_drop_percentages.append(culling_drop_percent)
+
+            culling_drop_percent_std = (
+                std_culling_with_intrusion / baseline_culling
+            ) * 100
+            std_culling_percentage.append(culling_drop_percent_std)
+
             intrusion_radii.append(intrusion_radius)
 
             # Calculate decreased area percentage
-            decreased_area_percent = (0.5 * np.pi * intrusion_radius**2 / total_area) * 100
+            decreased_area_percent = (
+                0.5 * np.pi * intrusion_radius**2 / total_area
+            ) * 100
             decreased_area_percentages.append(decreased_area_percent)
-
+        print(intrusion_radii)
     # Create the scatter plot
-    plt.figure(figsize=(8, 5))
+    plt.figure(figsize=(10, 5))
     # Plot culling drop percentages
-    plt.scatter(
-        intrusion_radii, culling_drop_percentages, color="blue", label="Culling Drop (%)"
+    # plt.errorbar(
+    #     intrusion_radii,
+    #     culling_drop_percentages,
+    #     std_culling_percentage,
+    #     color="blue",
+    #     label="Culling Drop (%)",
+    # )
+    plt.bar(
+        intrusion_radii,
+        culling_drop_percentages,
+        color="blue",
+        width=2,
+        label="Culling Drop (%)",
     )
     plt.scatter(
         intrusion_radii,
@@ -202,21 +439,23 @@ def create_culling_drop_scatter_plot(
     )
 
     plt.xlabel("Intrusion Radius")
-    plt.ylabel("Percentage Drop in Culling (%)")
+    plt.ylabel("Decreased Percentage (%)")
     plt.title("Culling Drop and Decreased Area vs Intrusion Radius")
+    plt.grid(axis="y", linestyle="--", alpha=0.6)
+    plt.xticks(intrusion_radii)
     plt.axhline(0, color="grey", linestyle="--", linewidth=1, alpha=0.8)
     plt.legend()
     plt.tight_layout()
 
     if is_save:
-        plt.savefig(image_folder_path + "culling_drop_vs_intrusion_radius.png")
+        plt.savefig(image_folder_path + "culling_drop_vs_intrusion_radius.svg")
 
     plt.show()
 
 
-
-    
 if __name__ == "__main__":
-    create_population_dynamics_multi_run(FOLDER_NAMES_LIST1, ROOT_PATH)
-    create_culling_statistics_multi_run(FOLDER_NAMES_LIST1, ROOT_PATH)
-    create_culling_drop_scatter_plot(FOLDER_NAMES_LIST3, ROOT_PATH3)
+    # create_population_dynamics_multi_run(FOLDER_NAMES_LIST1, ROOT_PATH)
+    # create_culling_statistics_multi_run(FOLDER_NAMES_LIST1, ROOT_PATH)
+    create_culling_drop_scatter_plot(
+        FOLDER_NAMES_LIST3, ROOT_PATH3, image_folder_path="./images/", is_save=True
+    )
